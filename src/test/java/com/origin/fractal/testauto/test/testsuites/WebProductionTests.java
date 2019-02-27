9611a1df3cc0bfc6762b8699f3690babe2a5533b
velwv <vel.winvinaya@gmail.com>
2019-02-27 09:51:31 +0530

    Report Enhancements

--- src/test/java/com/origin/fractal/testauto/test/testsuites/WebProductionTests.java
+++ src/test/java/com/origin/fractal/testauto/test/testsuites/WebProductionTests.java
@@ -17,7 +17,7 @@ public class WebProductionTests extends FractalBaseWebTest {
 		return new Object[][] {
 		  new Object[] { "1", "chrome" }
 		 , new Object[] { "2", "firefox" }
-		 , new Object[] { "3", "msedge" } ,
+		 , new Object[] { "3", "msedge" } 
 //		, new Object[] { "4", "ie11" }
 		};
 	}
