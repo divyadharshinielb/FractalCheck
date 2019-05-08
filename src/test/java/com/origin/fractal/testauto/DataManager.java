cedca90f2ce8216ff6f2f9064910788e657f5e4e
karpagavalli <karpagavalli.s@originlearning.com>
2019-05-08 18:28:28 +0530

    For jenkins setup in MAC system

--- src/test/java/com/origin/fractal/testauto/DataManager.java
+++ src/test/java/com/origin/fractal/testauto/DataManager.java
@@ -8,8 +8,9 @@ public class DataManager {
 	public static Object[][] browers() {
 		return new Object[][] {
 //			new Object[] { "1", "msedge" }, 
-				new Object[] { "2", "chrome" }
-//			,new Object[] { "3", "firefox" }
+				new Object[] { "2", "chrome" },
+				new Object[] { "3", "safari" }
+//			,new Object[] { "4", "firefox" }
 
 		};
 	}
