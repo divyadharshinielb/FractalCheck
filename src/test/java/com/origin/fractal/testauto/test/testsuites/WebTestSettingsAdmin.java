920977088f193220772a37e956a577212043d4c4
karpagavalli <karpagavalli.s@originlearning.com>
2019-07-04 11:26:53 +0530

    no message

--- src/test/java/com/origin/fractal/testauto/test/testsuites/WebTestSettingsAdmin.java
+++ src/test/java/com/origin/fractal/testauto/test/testsuites/WebTestSettingsAdmin.java
@@ -26,9 +26,9 @@ public void WebTestSettingsAdminPaymentConfig(String row, String strBrowserName)
 	    menuSteps.logout();
 		driver = BrowserFactory.getBrowser(strBrowserName);
 		login(driver);
-		HomeSteps homeSteps = new HomeSteps(driver);
+		/*HomeSteps homeSteps = new HomeSteps(driver);
 		homeSteps.verifyCartItemBtn1();
 		Reporter.writeSummary("TCID_008, Verify If there is one payment mode is available in admin ensure that payment mode is reflecting in user part., " +  homeSteps.getResult() );
-		Reporter.writeSummary("TCID_009, Verify User should able to access the payment and it should redirect to payment section," +  homeSteps.getResult() );    
+		Reporter.writeSummary("TCID_009, Verify User should able to access the payment and it should redirect to payment section," +  homeSteps.getResult() );   */ 
 	}
 }
