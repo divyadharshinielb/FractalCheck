88a013cb5017bc6a604c945d4b1f593cb06d8916
karpagavalli <karpagavalli.s@originlearning.com>
2019-08-29 16:43:44 +0530

    adding new apk

--- src/test/java/com/wv/auto/framework/app/AppFactory.java
+++ src/test/java/com/wv/auto/framework/app/AppFactory.java
@@ -15,7 +15,7 @@ import io.appium.java_client.ios.IOSDriver;
 public class AppFactory {
 
 	public static AppiumDriver<MobileElement> getAndroidApp() {
-		String fratalApkFile = "fractalapk/OriginFractal3.0.9.apk";
+		String fratalApkFile = "fractalapk/OriginFractal3.0.10.apk";
 		AppiumDriver<MobileElement> appDriver = null;
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
