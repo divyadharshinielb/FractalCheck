83559e06058b4e30e3a6fcd399b66bc43487b057
karpagavalli <karpagavalli.s@originlearning.com>
2019-09-18 15:58:25 +0530

    no message

--- src/test/java/com/origin/fractal/testauto/test/testsuites/WebTestsSettings.java
+++ src/test/java/com/origin/fractal/testauto/test/testsuites/WebTestsSettings.java
@@ -14,9 +14,9 @@ public class WebTestsSettings extends FractalBaseWebTest {
 	@DataProvider
 	public Object[][] browers() {
 		return new Object[][] { 
-//			new Object[] { "1", "chrome" } 
+			new Object[] { "1", "chrome" } 
 //		 new Object[] { "2", "firefox" } ,
-		new Object[] { "3", "msedge" } 
+//		new Object[] { "3", "msedge" } 
 //			new Object[] { "4", "ie11" }
 		};
 	}
