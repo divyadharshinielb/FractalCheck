bd899c7b21936b2c46bf84479b245eba2e2506c5
karpagavalli <karpagavalli.s@originlearning.com>
2019-10-10 14:46:23 +0530

    updated code

---
+++ src/test/java/com/origin/fractal/testauto/pages/ClassroomCCPage.java
@@ -0,0 +1,538 @@
+package com.origin.fractal.testauto.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.origin.fractal.testauto.FractalBasePage;
+
+public class ClassroomCCPage extends FractalBasePage {
+
+	private By Classroomicon = By.xpath("//div[@class='icon-Classroom-icon icon']");
+	private By AddClassbtn = By.xpath("//html/body/div/div/div[2]/div[3]/div[3]/div/div/div/div[2]/button");
+
+	//Add classroom
+	private By lblCCAllsession = By.xpath("//div[@class='col-lg-5 col-sm-5 col-xs-5 col-md-5 float-right padding-l-0 padding-t-60']//div[2]//label[1]");
+	private By lblCCAssDuedate = By.xpath("//*[contains(@class, 'col-sm-5')]/div[3]/label");
+	private By lblCCQualifyPrecentage = By.xpath("//html/body/div[3]/div/div/div/md-dialog-content/div/div/form/div/div[4]/div/div[2]/div[4]/label");
+	private By InputCCQualifyPrecentage = By.xpath("//html/body/div[3]/div/div/div/md-dialog-content/div/div/form/div/div[4]/div/div[2]/div[4]/label/div/div/md-input-container/input");
+	private By lblCCFeedbackDuedate = By.xpath("//div[contains(@class,'col-lg-5 col-sm-5 col-xs-5 col-md-5 float-right padding-l-0 padding-t-60')]//div[5]//label[1]");
+	private By lblCCManualCompletion= By.xpath("//div[6]//label[1]");
+	private By Cancelbtn= By.xpath("//button[@class='btn btn-cancel btn_radius ng-binding text-left']");
+
+	//Add classroom cc text
+	private By lblCCAllsessiontxt= By.xpath("/html[1]/body[1]/div[3]/div[1]/div[1]/div[1]/md-dialog-content[1]/div[1]/div[1]/form[1]/div[1]/div[4]/div[1]/div[2]/div[2]/label[1]");
+	private By lblCCAssDuedatetxt = By.xpath("/html[1]/body[1]/div[3]/div[1]/div[1]/div[1]/md-dialog-content[1]/div[1]/div[1]/form[1]/div[1]/div[4]/div[1]/div[2]/div[3]/label[1]");
+	private By lblCCQualifyPrecentagetxt = By.xpath("/html[1]/body[1]/div[3]/div[1]/div[1]/div[1]/md-dialog-content[1]/div[1]/div[1]/form[1]/div[1]/div[4]/div[1]/div[2]/div[4]/label[1]");
+	private By lblCCFeedbackDuedatetxt = By.xpath("/html[1]/body[1]/div[3]/div[1]/div[1]/div[1]/md-dialog-content[1]/div[1]/div[1]/form[1]/div[1]/div[4]/div[1]/div[2]/div[5]/label[1]");
+	private By lblCCManualCompletiontxt= By.xpath("/html[1]/body[1]/div[3]/div[1]/div[1]/div[1]/md-dialog-content[1]/div[1]/div[1]/form[1]/div[1]/div[4]/div[1]/div[2]/div[6]/label[1]");
+
+	//Edit classroom
+	private By EditClassroomicon = By.xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[3]/div[3]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[5]/i[2]");
+	private By EditclasslblCCAllsession = By.xpath("/html[1]/body[1]/div[3]/div[1]/div[1]/div[1]/md-dialog-content[1]/div[1]/div[1]/form[1]/div[1]/div[4]/div[1]/div[2]/div[2]/label[1]");
+	private By EditclasslblCCAssDuedate = By.xpath("/html[1]/body[1]/div[3]/div[1]/div[1]/div[1]/md-dialog-content[1]/div[1]/div[1]/form[1]/div[1]/div[4]/div[1]/div[2]/div[3]/label[1]");
+	private By EditclasslblCCQualifyPrecentage = By.xpath("/html[1]/body[1]/div[3]/div[1]/div[1]/div[1]/md-dialog-content[1]/div[1]/div[1]/form[1]/div[1]/div[4]/div[1]/div[2]/div[4]/label[1]");
+	private By EditclassInputCCQualifyPrecentage = By.xpath("/html[1]/body[1]/div[3]/div[1]/div[1]/div[1]/md-dialog-content[1]/div[1]/div[1]/form[1]/div[1]/div[4]/div[1]/div[2]/div[4]/label[1]/div[1]/div[1]/md-input-container[1]/input[1]");
+	private By EditclasslblCCFeedbackDuedate = By.xpath("/html[1]/body[1]/div[3]/div[1]/div[1]/div[1]/md-dialog-content[1]/div[1]/div[1]/form[1]/div[1]/div[4]/div[1]/div[2]/div[5]/label[1]");
+	private By EditclasslblCCManualCompletion= By.xpath("/html[1]/body[1]/div[3]/div[1]/div[1]/div[1]/md-dialog-content[1]/div[1]/div[1]/form[1]/div[1]/div[4]/div[1]/div[2]/div[6]/label[1]");
+	private By EditclassCancelbtn= By.xpath("/html[1]/body[1]/div[3]/div[1]/div[1]/div[1]/md-dialog-content[1]/div[1]/div[1]/form[1]/div[1]/div[5]/div[2]/span[1]/div[1]/button[1]");
+
+
+	private By HeaderAClogo= By.xpath("//span[contains(@class,'loggeduserbg ng-binding')]");
+	private By logout= By.xpath("//html/body/div/div/div/ng-include/div/div/div[2]/div/div/ul/div/div[3]");
+	public boolean booleanresult;
+
+	public ClassroomCCPage(WebDriver driver) 
+	{
+		super(driver);
+		pageName ="ClassroomPage"; 
+	}
+
+	public void GotoClassroom() 
+	{
+		click(Classroomicon);
+		wait(5);
+		print("In Admin side, Classroom is opened");
+	}
+
+	public void GotoEditClassroom() 
+	{
+		wait(5);
+		click(EditClassroomicon);
+		wait(5);
+		print("In Admin side, Edit Classroom is opened");
+	}
+
+	public void ClickCClblAllsession() 
+	{
+		click(AddClassbtn);
+		wait(5);
+		click(lblCCAllsession);
+		wait(5);
+		click(Cancelbtn);
+		wait(5);
+		
+	}
+
+	public void ClickEditCClblAllsession() 
+	{
+
+		click(EditclasslblCCAllsession);
+		wait(5);
+		click(EditclassCancelbtn);
+		wait(5);
+		
+	}
+
+
+	public void ClickCClblAssDuedate() 
+	{
+		click(AddClassbtn);
+		wait(5);
+		click(lblCCAssDuedate);
+		wait(5);
+		click(Cancelbtn);
+		wait(5);
+	
+	}
+
+	public void ClickEditCClblAssDuedate() 
+	{
+		click(EditclasslblCCAssDuedate);
+		wait(5);
+		click(EditclassCancelbtn);
+		wait(5);
+		
+	}
+
+
+	public void ClickCClblQualifyPrecentage() 
+	{
+		click(AddClassbtn);
+		wait(5);
+		click(lblCCQualifyPrecentage);
+		wait(5);
+		enterData("35",InputCCQualifyPrecentage);
+		wait(5);
+		click(Cancelbtn);
+		wait(5);
+		
+	}
+
+	public void ClickEditCClblQualifyPrecentage() 
+	{
+		click(EditclasslblCCQualifyPrecentage);
+		wait(5);
+		clear(EditclassInputCCQualifyPrecentage);
+		enterData("35",EditclassInputCCQualifyPrecentage);
+		wait(5);
+		click(EditclassCancelbtn);
+		wait(5);
+	
+	}
+
+	public void ClickCClblFeedbackDuedate() 
+	{
+		click(AddClassbtn);
+		wait(5);
+		click(lblCCFeedbackDuedate);
+		wait(5);
+		click(Cancelbtn);
+		wait(5);
+		
+	}
+	public void ClickEditCClblFeedbackDuedate() 
+	{
+		click(EditclasslblCCFeedbackDuedate);
+		wait(5);
+		click(EditclassCancelbtn);
+		wait(5);
+	
+	}
+
+	public void ClickCClblManualCompletion() 
+	{
+		click(AddClassbtn);
+		wait(5);
+		click(lblCCManualCompletion);
+		wait(5);
+		click(Cancelbtn);
+		wait(5);
+		
+	}
+	public void ClickEditCClblManualCompletion() 
+	{
+		click(EditclasslblCCManualCompletion);
+		wait(5);
+		click(EditclassCancelbtn);
+		wait(5);
+	}
+
+	public void ClickCClblAllSession_Subdate() 
+	{
+		click(AddClassbtn);
+		wait(5);
+		click(lblCCAllsession);
+		wait(5);
+		click(lblCCAssDuedate);
+		wait(5);
+		click(Cancelbtn);
+		wait(5);
+		
+	}
+
+	public void ClickEditCClblAllSession_Subdate() 
+	{
+		click(EditclasslblCCAllsession);
+		wait(5);
+		click(EditclasslblCCAssDuedate);
+		wait(5);
+		click(EditclassCancelbtn);
+		wait(5);
+		
+	}
+
+	public void ClickCClblSubdate_Qualify() 
+	{
+		click(AddClassbtn);
+		wait(5);
+		click(lblCCAssDuedate);
+		wait(5);
+		click(lblCCQualifyPrecentage);
+		wait(5);
+		enterData("35",EditclassInputCCQualifyPrecentage);
+		wait(5);
+		click(Cancelbtn);
+		wait(5);
+		
+	}
+
+	public void ClickEditCClblSubdate_Qualify() 
+	{
+		click(EditclasslblCCAssDuedate);
+		wait(5);
+		click(EditclasslblCCQualifyPrecentage);
+		wait(5);
+		enterData("35",EditclassInputCCQualifyPrecentage);
+		wait(5);
+		click(EditclassCancelbtn);
+		wait(5);
+		
+	}
+
+
+	public void ClickCClblQualify_feedbackdate() 
+	{
+		click(AddClassbtn);
+		wait(5);
+		click(lblCCQualifyPrecentage);
+		wait(5);
+		enterData("35",InputCCQualifyPrecentage);
+		wait(5);
+		click(lblCCFeedbackDuedate);
+		wait(5);
+		click(Cancelbtn);
+		wait(5);
+		
+	}
+
+	public void ClickEditCClblQualify_feedbackdate() 
+	{
+		click(EditclasslblCCQualifyPrecentage);
+		wait(5);
+		enterData("35",EditclassInputCCQualifyPrecentage);
+		wait(5);
+		click(EditclasslblCCFeedbackDuedate);
+		wait(5);
+		click(Cancelbtn);
+		wait(5);
+		
+	}
+
+	public void ClickCClblfeedbackdate_Manualcomp() 
+	{
+		click(AddClassbtn);
+		wait(5);
+		click(lblCCFeedbackDuedate);
+		wait(5);
+		click(lblCCManualCompletion);
+		wait(5);
+		click(Cancelbtn);
+		wait(5);
+		
+	}
+
+	public void ClickEditCClblfeedbackdate_Manualcomp() 
+	{
+		click(EditclasslblCCFeedbackDuedate);
+		wait(5);
+		click(EditclasslblCCManualCompletion);
+		wait(5);
+		click(EditclassCancelbtn);
+		wait(5);
+		
+	}
+
+	public void ClickCClblManualcomp_Allsession() 
+	{
+		click(AddClassbtn);
+		wait(5);
+		click(lblCCManualCompletion);
+		wait(5);
+		click(lblCCAllsession);
+		wait(5);
+		click(Cancelbtn);
+		wait(5);
+		
+	}
+
+	public void ClickEditCClblManualcomp_Allsession() 
+	{
+		click(EditclasslblCCManualCompletion);
+		wait(5);
+		click(EditclasslblCCAllsession);
+		wait(5);
+		click(EditclassCancelbtn);
+		wait(5);
+		
+	}
+
+	public boolean verifyCCTexts() {
+		click(AddClassbtn);
+		wait(5);
+		boolean  boleanresult1=false, boleanresult2=false, boleanresult3 =false, boleanresult4 =false, boleanresult5 =false;
+		if (verifyText("Participation in all sessions",lblCCAllsessiontxt) == true)
+		{
+			boleanresult1 = true;
+		}
+		if (verifyText("Submission of all assignments before due date",lblCCAssDuedatetxt)==true)
+		{
+			boleanresult2 = true;
+		}
+		if(verifyText("% Qualifying score in graded assignments",lblCCQualifyPrecentagetxt) == true)
+		{
+			boleanresult3 = true;
+		}
+		if(verifyText("Submission of session/event feedback before due date",lblCCFeedbackDuedatetxt)== true)
+		{
+			boleanresult4 = true;
+		}
+		if(verifyText("Manual completion marking",lblCCManualCompletiontxt)== true) {
+			boleanresult5 = true;
+		}
+
+		booleanresult= boleanresult1 && boleanresult2 && boleanresult3 && boleanresult4 && boleanresult5;
+		System.out.println("Boolean Result : "+booleanresult); // this is boolean result
+		click(Cancelbtn);
+		wait(5);
+		return booleanresult;
+	}
+
+	public boolean verifyEditCCTexts() {
+		
+		booleanresult= false;
+		boolean  boleanresult1=false, boleanresult2=false, boleanresult3 =false, boleanresult4 =false, boleanresult5 =false;
+		if (verifyText("Participation in all sessions",EditclasslblCCAllsession) == true)
+		{
+			boleanresult1 = true;
+		}
+		if (verifyText("Submission of all assignments before due date",EditclasslblCCAssDuedate)==true)
+		{
+			boleanresult2 = true;
+		}
+		if(verifyText("% Qualifying score in graded assignments",EditclasslblCCQualifyPrecentage) == true)
+		{
+			boleanresult3 = true;
+		}
+		if(verifyText("Submission of session/event feedback before due date",EditclasslblCCFeedbackDuedate)== true)
+		{
+			boleanresult4 = true;
+		}
+		if(verifyText("Manual completion marking",EditclasslblCCManualCompletion)== true) {
+			boleanresult5 = true;
+		}
+
+		booleanresult= boleanresult1 && boleanresult2 && boleanresult3 && boleanresult4 && boleanresult5;
+		System.out.println("Boolean Result : "+booleanresult); // this is boolean result
+		click(EditclassCancelbtn);
+		wait(5);
+		return booleanresult;
+	}
+
+
+
+	public boolean verifyCCsessionLabels() {
+		wait(5);
+		click(AddClassbtn);
+		wait(5);
+		booleanresult = false;
+		if (verifyText("Participation in all sessions",lblCCAllsessiontxt) == true)
+		{
+			booleanresult = true;
+		}
+		click(Cancelbtn);
+		wait(5);
+		return booleanresult;
+	}
+	
+	public boolean verifyEditCCsessionLabels() {
+		booleanresult = false;
+		if (verifyText("Participation in all sessions",EditclasslblCCAllsession) == true)
+		{
+			booleanresult = true;
+		}
+		click(EditclassCancelbtn);
+		wait(5);
+		return booleanresult;
+	}
+
+	public boolean verifyCCAssDuedateLabels() {
+		wait(5);
+		click(AddClassbtn);
+		wait(5);
+		booleanresult = false;
+		if (verifyText("Submission of all assignments before due date",lblCCAssDuedatetxt) == true)
+		{
+			booleanresult = true;
+		}
+		click(Cancelbtn);
+		wait(5);
+		return booleanresult;
+	}
+	
+	public boolean verifyEditCCAssDuedateLabels() {
+		
+		booleanresult = false;
+		if (verifyText("Submission of all assignments before due date",EditclasslblCCAssDuedate) == true)
+		{
+			booleanresult = true;
+		}
+		click(EditclassCancelbtn);
+		wait(5);
+		return booleanresult;
+	}
+	
+	public boolean verifyCCQualifyPrecentageLabels() {
+		wait(5);
+		click(AddClassbtn);
+		wait(5);
+		booleanresult = false;
+		if (verifyText("% Qualifying score in graded assignments",lblCCQualifyPrecentagetxt) == true)
+		{
+			booleanresult = true;
+		}
+		click(Cancelbtn);
+		wait(5);
+		return booleanresult;
+	}
+	
+	public boolean verifyEditCCQualifyPrecentageLabels() {
+		
+		booleanresult = false;
+		if (verifyText("% Qualifying score in graded assignments",EditclasslblCCQualifyPrecentage) == true)
+		{
+			booleanresult = true;
+		}
+		click(EditclassCancelbtn);
+		wait(5);
+		return booleanresult;
+	}
+	
+	public boolean verifyCCFeedbackDuedateLabels() {
+		wait(5);
+		click(AddClassbtn);
+		wait(5);
+		booleanresult = false;
+		if (verifyText("Submission of session/event feedback before due date",lblCCFeedbackDuedatetxt) == true)
+		{
+			booleanresult = true;
+		}
+		click(Cancelbtn);
+		wait(5);
+		return booleanresult;
+	}
+	
+	public boolean verifyEditCCFeedbackDuedateLabels() {
+		booleanresult = false;
+		if (verifyText("Submission of session/event feedback before due date",EditclasslblCCFeedbackDuedate) == true)
+		{
+			booleanresult = true;
+		}
+		click(EditclassCancelbtn);
+		wait(5);
+		return booleanresult;
+	}
+	
+	public boolean verifyCCManualCompletionLabels() {
+		click(AddClassbtn);
+		wait(5);
+		booleanresult = false;
+		if (verifyText("Manual completion marking",lblCCManualCompletiontxt) == true)
+		{
+			booleanresult = true;
+		}
+		click(Cancelbtn);
+		wait(5);
+		return booleanresult;
+	}
+	
+	public boolean verifyEditCCManualCompletionLabels() {
+		booleanresult = false;
+		if (verifyText("Manual completion marking",EditclasslblCCManualCompletion) == true)
+		{
+			booleanresult = true;
+		}
+		click(EditclassCancelbtn);
+		wait(5);
+		return booleanresult;
+	}
+
+	// Qualifying score allows single digit
+	public void ClickCClblQualifySinglePrecentage() 
+	{
+		click(AddClassbtn);
+		wait(5);
+		click(lblCCQualifyPrecentage);
+		wait(5);
+		enterData("5",InputCCQualifyPrecentage);
+		wait(5);
+		click(Cancelbtn);
+		wait(5);
+		click(AddClassbtn);
+		wait(5);
+		enterData("5",InputCCQualifyPrecentage);
+		wait(5);
+		click(lblCCQualifyPrecentage);
+		wait(5);
+		click(Cancelbtn);
+		wait(5);
+	}
+	
+	// Qualifying score allows single digit
+	public void EditCClblQualifySinglePrecentage() 
+	{
+		
+		click(EditclasslblCCQualifyPrecentage);
+		wait(5);
+		enterData("5",EditclassInputCCQualifyPrecentage);
+		wait(5);
+		click(EditclassCancelbtn);
+		wait(5);
+		click(EditClassroomicon);
+		wait(5);
+		enterData("5",EditclassInputCCQualifyPrecentage);
+		wait(5);
+		click(EditclasslblCCQualifyPrecentage);
+		wait(5);
+		click(EditclassCancelbtn);
+		wait(5);
+		
+	}
+
+	public void Clicklogout() 
+	{
+		click(HeaderAClogo);
+		wait(5);
+		click(logout);
+		wait(5);
+	}
+}
---
+++ src/test/java/com/origin/fractal/testauto/steps/ClassroomCCSteps.java
@@ -0,0 +1,593 @@
+package com.origin.fractal.testauto.steps;
+
+import org.openqa.selenium.WebDriver;
+
+import com.origin.fractal.testauto.FractalBaseStep;
+import com.origin.fractal.testauto.pages.ClassroomCCPage;
+
+public class ClassroomCCSteps extends FractalBaseStep {
+	private ClassroomCCPage page;
+
+	public ClassroomCCSteps(WebDriver driver) {
+		page = new ClassroomCCPage(driver);
+		fBPage = page;
+	}
+
+	public void GotoClassroom() {
+		result ="PASSED";
+		try {
+			page.GotoClassroom();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void GotoEditClassroom() {
+		result ="PASSED";
+		try {
+			page.GotoEditClassroom();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void CClblAllsession() {
+		result ="PASSED";
+		try {
+			page.ClickCClblAllsession();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void EditCClblAllsession() {
+		result ="PASSED";
+		try {
+			page.ClickEditCClblAllsession();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void CClblAssDuedate() {
+		result ="PASSED";
+		try {
+			page.ClickCClblAssDuedate();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void EditCClblAssDuedate() {
+		result ="PASSED";
+		try {
+			page.ClickEditCClblAssDuedate();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void CClblQualifyPrecentage() {
+		result ="PASSED";
+		try {
+			page.ClickCClblQualifyPrecentage();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void EditCClblQualifyPrecentage() {
+		result ="PASSED";
+		try {
+			page.ClickEditCClblQualifyPrecentage();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void CClblFeedbackDuedate() {
+		result ="PASSED";
+		try {
+			page.ClickCClblFeedbackDuedate();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void EditCClblFeedbackDuedate() {
+		result ="PASSED";
+		try {
+			page.ClickEditCClblFeedbackDuedate();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+
+	public void CClblManualCompletion() {
+		result ="PASSED";
+		try {
+			page.ClickCClblManualCompletion();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void EditCClblManualCompletion() {
+		result ="PASSED";
+		try {
+			page.ClickEditCClblManualCompletion();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void CClblAllSession_Subdate() {
+		result ="PASSED";
+		try {
+			page.ClickCClblAllSession_Subdate();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void EditCClblAllSession_Subdate() {
+		result ="PASSED";
+		try {
+			page.ClickEditCClblAllSession_Subdate();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void CClblSubdate_Qualify() {
+		result ="PASSED";
+		try {
+			page.ClickCClblSubdate_Qualify();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void EditCClblSubdate_Qualify() {
+		result ="PASSED";
+		try {
+			page.ClickEditCClblSubdate_Qualify();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void CClblQualify_feedbackdate() {
+		result ="PASSED";
+		try {
+			page.ClickCClblQualify_feedbackdate();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void EditCClblQualify_feedbackdate() {
+		result ="PASSED";
+		try {
+			page.ClickEditCClblQualify_feedbackdate();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void CClblfeedbackdate_Manualcomp() {
+		result ="PASSED";
+		try {
+			page.ClickCClblfeedbackdate_Manualcomp();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void EditCClblfeedbackdate_Manualcomp() {
+		result ="PASSED";
+		try {
+			page.ClickEditCClblfeedbackdate_Manualcomp();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void CClblManualcomp_Allsession() {
+		result ="PASSED";
+		try {
+			page.ClickCClblManualcomp_Allsession();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+	public void EditCClblManualcomp_Allsession() {
+		result ="PASSED";
+		try {
+			page.ClickEditCClblManualcomp_Allsession();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	// Overall CC text
+	public void verifyCCTexts() {
+		System.out.println("Result boolean : "+ page.booleanresult); //booleanresult
+		try {
+			page.verifyCCTexts();
+			if (page.booleanresult == true) {
+				result ="PASSED";
+
+			}
+			else if (page.booleanresult == false){
+				result ="FAILED";
+			}
+			else {
+				result = "FAILED";
+				// log - There is a problem in verifyCCTexts()
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+
+	}
+
+	public void verifyEditCCTexts() {
+		System.out.println("Result boolean : "+ page.booleanresult); //booleanresult
+		try {
+			page.verifyEditCCTexts();
+			if (page.booleanresult == true) {
+				result ="PASSED";
+
+			}
+			else if (page.booleanresult == false){
+				result ="FAILED";
+			}
+			else {
+				result = "FAILED";
+				// log - There is a problem in verifyCCTexts()
+			}
+		}
+
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+
+	}
+
+	public void verifyCCsessionLabels() {
+
+		try {
+			page.verifyCCsessionLabels();
+			if (page.booleanresult == true) {
+				result ="PASSED";
+
+			}
+			else if (page.booleanresult == false){
+				result ="FAILED";
+			}
+			else {
+				result = "FAILED";
+				// log - There is a problem in verifyCCTexts()
+			}
+		}
+
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}	}
+
+	public void verifyEditCCsessionLabels() {
+
+		try {
+			page.verifyEditCCsessionLabels();
+			if (page.booleanresult == true) {
+				result ="PASSED";
+
+			}
+			else if (page.booleanresult == false){
+				result ="FAILED";
+			}
+			else {
+				result = "FAILED";
+				// log - There is a problem in verifyCCTexts()
+			}
+		}
+
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}	}
+
+	public void verifyCCAssDuedateLabels() {
+		try {
+			page.verifyCCAssDuedateLabels();
+			if (page.booleanresult == true) {
+				result ="PASSED";
+
+			}
+			else if (page.booleanresult == false){
+				result ="FAILED";
+			}
+			else {
+				result = "FAILED";
+				// log - There is a problem in verifyCCTexts()
+			}
+		}
+
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}		}
+
+	public void verifyEditCCAssDuedateLabels() {
+		try {
+			page.verifyEditCCAssDuedateLabels();
+			if (page.booleanresult == true) {
+				result ="PASSED";
+
+			}
+			else if (page.booleanresult == false){
+				result ="FAILED";
+			}
+			else {
+				result = "FAILED";
+				// log - There is a problem in verifyCCTexts()
+			}
+		}
+
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}		}
+
+	public void verifyCCQualifyPrecentageLabels() {
+		try {
+			page.verifyCCQualifyPrecentageLabels();
+			if (page.booleanresult == true) {
+				result ="PASSED";
+			}
+			else if (page.booleanresult == false){
+				result ="FAILED";
+			}
+			else {
+				result = "FAILED";
+				// log - There is a problem in verifyCCTexts()
+			}
+		}
+
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}	}
+
+	public void verifyEditCCQualifyPrecentageLabels() {
+		try {
+			page.verifyEditCCQualifyPrecentageLabels();
+			if (page.booleanresult == true) {
+				result ="PASSED";
+			}
+			else if (page.booleanresult == false){
+				result ="FAILED";
+			}
+			else {
+				result = "FAILED";
+				// log - There is a problem in verifyCCTexts()
+			}
+		}
+
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}	}
+
+
+	public void verifyCCFeedbackDuedateLabels() {
+		try {
+			page.verifyCCFeedbackDuedateLabels();
+			if (page.booleanresult == true) {
+				result ="PASSED";
+			}
+			else if (page.booleanresult == false){
+				result ="FAILED";
+			}
+			else {
+				result = "FAILED";
+				// log - There is a problem in verifyCCTexts()
+			}
+		}
+
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void verifyEditCCFeedbackDuedateLabels() {
+		try {
+			page.verifyEditCCFeedbackDuedateLabels();
+			if (page.booleanresult == true) {
+				result ="PASSED";
+			}
+			else if (page.booleanresult == false){
+				result ="FAILED";
+			}
+			else {
+				result = "FAILED";
+				// log - There is a problem in verifyCCTexts()
+			}
+		}
+
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+
+	public void verifyCCManualCompletionLabels() {
+		try {
+			page.verifyCCManualCompletionLabels();
+			if (page.booleanresult == true) {
+				result ="PASSED";
+
+			}
+			else if (page.booleanresult == false){
+				result ="FAILED";
+			}
+			else {
+				//page.Else();
+				result = "FAILED";
+				// log - There is a problem in verifyCCTexts()
+			}
+		}
+
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	public void verifyEditCCManualCompletionLabels() {
+		try {
+			page.verifyEditCCManualCompletionLabels();
+			if (page.booleanresult == true) {
+				result ="PASSED";
+
+			}
+			else if (page.booleanresult == false){
+				result ="FAILED";
+			}
+			else {
+				//page.Else();
+				result = "FAILED";
+				// log - There is a problem in verifyCCTexts()
+			}
+		}
+
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+
+	public void Clicklogout() {
+		result ="PASSED";
+		try {
+			page.Clicklogout();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+
+	// 25th TC in manual testing
+	public void CClblQualifySinglePrecentage() {
+		result ="PASSED";
+		try {
+			page.ClickCClblQualifySinglePrecentage();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+	// 25th TC in manual testing
+	public void EditCClblQualifySinglePrecentage() {
+		result ="PASSED";
+		try {
+			page.EditCClblQualifySinglePrecentage();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();   
+			result="FAILED";
+		}
+	}
+}
