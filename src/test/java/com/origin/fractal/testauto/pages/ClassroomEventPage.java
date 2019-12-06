70784ebf93ea11dc07875b931ef0dbfbda4afb82
Mahesh_Geddam <mahesh.g@originlearning.com>
2019-12-06 11:01:34 +0530

    no message

--- src/test/java/com/origin/fractal/testauto/pages/ClassroomEventPage.java
+++ src/test/java/com/origin/fractal/testauto/pages/ClassroomEventPage.java
@@ -584,6 +584,7 @@ public class ClassroomEventPage extends FractalBasePage{
 	click(lblSelectCalendar);
 	wait(5);
 	click(dropdownSelectCalendar);
+	wait(5);
 	enterData("10",lblCalendarHour);
 	enterData("50",lblCalendarMinute);
 	click(lblPm);
