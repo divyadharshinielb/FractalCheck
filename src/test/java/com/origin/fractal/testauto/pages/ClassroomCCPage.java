8c6c124c6f54c9c0d2559b4a10699aa87bf088f2
Mahesh_Geddam <mahesh.g@originlearning.com>
2019-12-20 12:52:59 +0530

    no message

--- src/test/java/com/origin/fractal/testauto/pages/ClassroomCCPage.java
+++ src/test/java/com/origin/fractal/testauto/pages/ClassroomCCPage.java
@@ -301,7 +301,7 @@ public class ClassroomCCPage extends FractalBasePage {
 	}
 
 	public boolean verifyCCTexts() {
-	//	click(AddClassbtn);
+	//	click(AddClassbtn); /**added by mahesh 20-12-19
 		wait(5);
 		boolean  boleanresult1=false, boleanresult2=false, boleanresult3 =false, boleanresult4 =false, boleanresult5 =false;
 /*		if (verifyText("Participation in all sessions",lblCCAllsessiontxt) == false)
