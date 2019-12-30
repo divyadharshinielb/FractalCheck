96054238f4105d4299753e25917596289591e54e
karpagavalli <karpagavalli.s@originlearning.com>
2019-12-30 11:28:03 +0530

    issues fixes

--- src/test/java/com/wv/auto/framework/app/AppFactory.java
+++ src/test/java/com/wv/auto/framework/app/AppFactory.java
@@ -15,7 +15,7 @@ import io.appium.java_client.ios.IOSDriver;
 public class AppFactory {
 
 	public static AppiumDriver<MobileElement> getAndroidApp() {
-		String fratalApkFile = "fractalapk/OriginFractal3.0.14.apk";
+		String fratalApkFile = "fractalapk/OriginFractal3.0.16.apk";
 		AppiumDriver<MobileElement> appDriver = null;
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
