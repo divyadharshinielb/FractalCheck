a0ffcd773adbbac6cf4def015a8457db5ee65c8f
karpagavalli <karpagavalli.s@originlearning.com>
2020-02-11 12:37:27 +0530

    test case id updated for roles management

--- src/test/java/com/origin/fractal/testauto/test/testsuites/WebTestRolesManagement.java
+++ src/test/java/com/origin/fractal/testauto/test/testsuites/WebTestRolesManagement.java
@@ -28,25 +28,25 @@ public class WebTestRolesManagement extends FractalBaseWebTest {
 	loginToContentAdmin(driver);
 		rolesmngmtsteps.clickMenu();
 		rolesmngmtsteps.clickClassroom();
-		Reporter.writeSummary("TCID_0013, Verify navigation of classroom page, " +  rolesmngmtsteps.getResult() );
+		Reporter.writeSummary("TCID_RolesManagement_0013, Verify navigation of classroom page, " +  rolesmngmtsteps.getResult() );
 		rolesmngmtsteps.addclassroomcheck();
-		Reporter.writeSummary("TCID_0014, Verify instructors present in Add classroom, " +  rolesmngmtsteps.getResult() );
+		Reporter.writeSummary("TCID_RolesManagement_0014, Verify instructors present in Add classroom, " +  rolesmngmtsteps.getResult() );
 		rolesmngmtsteps.editclassroomcheck();
-		Reporter.writeSummary("TCID_0015, Verify instructors present in Edit classroom, " +  rolesmngmtsteps.getResult() );
+		Reporter.writeSummary("TCID_RolesManagement_0015, Verify instructors present in Edit classroom, " +  rolesmngmtsteps.getResult() );
 		rolesmngmtsteps.editmanagerolefunctionality();
 		rolesmngmtsteps.gotoManageUser();
 		rolesmngmtsteps.BusinessUserRole();
-		Reporter.writeSummary("TCID_001, Verify roles mapped to business user in admin part, " +  rolesmngmtsteps.getResult() );
+		Reporter.writeSummary("TCID_RolesManagement_001, Verify roles mapped to business user in admin part, " +  rolesmngmtsteps.getResult() );
 		rolesmngmtsteps.instructorUserRole();
-		Reporter.writeSummary("TCID_002, Verify roles mapped to instructor user in admin part, " +  rolesmngmtsteps.getResult() );
+		Reporter.writeSummary("TCID_RolesManagement_002, Verify roles mapped to instructor user in admin part, " +  rolesmngmtsteps.getResult() );
 	rolesmngmtsteps.businessAdminRole();
-		Reporter.writeSummary("TCID_003, Verify roles mapped to businessAdmin in admin part, " +  rolesmngmtsteps.getResult() );
+		Reporter.writeSummary("TCID_RolesManagement_003, Verify roles mapped to businessAdmin in admin part, " +  rolesmngmtsteps.getResult() );
 		rolesmngmtsteps.siteAdminRole();
-		Reporter.writeSummary("TCID_004, Verify roles mapped to siteAdmin in admin part, " +  rolesmngmtsteps.getResult() );
+		Reporter.writeSummary("TCID_RolesManagement_004, Verify roles mapped to siteAdmin in admin part, " +  rolesmngmtsteps.getResult() );
 		rolesmngmtsteps.directUserRole();
-		Reporter.writeSummary("TCID_005, Verify roles mapped to directUser in admin part, " +  rolesmngmtsteps.getResult() );
+		Reporter.writeSummary("TCID_RolesManagement_005, Verify roles mapped to directUser in admin part, " +  rolesmngmtsteps.getResult() );
 		rolesmngmtsteps.learningAdminRole();
-		Reporter.writeSummary("TCID_006, Verify roles mapped to learningAdmin in admin part, " +  rolesmngmtsteps.getResult() );
+		Reporter.writeSummary("TCID_RolesManagement_006, Verify roles mapped to learningAdmin in admin part, " +  rolesmngmtsteps.getResult() );
 	
 	}
 	@Test(dataProviderClass=DataManager.class, dataProvider = "browers", groups = { "Phase1.0" }, enabled = true, description = "Roles")
@@ -54,19 +54,19 @@ public class WebTestRolesManagement extends FractalBaseWebTest {
 		driver = BrowserFactory.getBrowser(strBrowserName);
 		RolesManagementSteps rolesmngmtsteps = new RolesManagementSteps(driver);
 	rolesmngmtsteps.DirectuserwithLearningadminandinsrole();
-	   Reporter.writeSummary("TCID_007, Verify direct user with learning admin and instructor role, " +  rolesmngmtsteps.getResult() );
+	   Reporter.writeSummary("TCID_RolesManagement_007, Verify direct user with learning admin and instructor role, " +  rolesmngmtsteps.getResult() );
 		rolesmngmtsteps.Checkdirectuserinsandlearningadminmapping();
-		Reporter.writeSummary("TCID_008, Verify direct user mapping with learning admin and instructor role, " +  rolesmngmtsteps.getResult() );
+		Reporter.writeSummary("TCID_RolesManagement_008, Verify direct user mapping with learning admin and instructor role, " +  rolesmngmtsteps.getResult() );
 	   rolesmngmtsteps.InstructorwithDirectuserrole();
-		Reporter.writeSummary("TCID_009, Verify Instructor with Directuser role mapping, " +  rolesmngmtsteps.getResult() );
+		Reporter.writeSummary("TCID_RolesManagement_009, Verify Instructor with Directuser role mapping, " +  rolesmngmtsteps.getResult() );
 		rolesmngmtsteps.SiteadminwithDirectuserRole();
-		Reporter.writeSummary("TCID_010, Verify siteadmin with direct role mapping, " + rolesmngmtsteps.getResult() );
+		Reporter.writeSummary("TCID_RolesManagement_010, Verify siteadmin with direct role mapping, " + rolesmngmtsteps.getResult() );
 		rolesmngmtsteps.LearningadminwithDirectuserrole();
-		Reporter.writeSummary("TCID_011, Verify learning admin with Direct role mapping, " +  rolesmngmtsteps.getResult() );
+		Reporter.writeSummary("TCID_RolesManagement_011, Verify learning admin with Direct role mapping, " +  rolesmngmtsteps.getResult() );
 		rolesmngmtsteps.BusinessadminwithbusinessuserRole();
-		Reporter.writeSummary("TCID_012, Verify business admin with Business user, " +  rolesmngmtsteps.getResult() );
+		Reporter.writeSummary("TCID_RolesManagement_012, Verify business admin with Business user, " +  rolesmngmtsteps.getResult() );
 		rolesmngmtsteps.Businessuserwithinsrole();
-		Reporter.writeSummary("TCID_013, Verify Business user with Instructor role, " +  rolesmngmtsteps.getResult() );
+		Reporter.writeSummary("TCID_RolesManagement_013, Verify Business user with Instructor role, " +  rolesmngmtsteps.getResult() );
 		
 		
 	}
