68b1c1cc6517aa4a53d28479f839d478fe46889e
Mahesh_Geddam <mahesh.g@originlearning.com>
2020-08-07 16:09:31 +0530

    updated video test scripts

--- src/test/java/com/origin/fractal/testauto/pages/videoTesterPage.java
+++ src/test/java/com/origin/fractal/testauto/pages/videoTesterPage.java
@@ -15,6 +15,14 @@ public class videoTesterPage extends FractalBasePage{
 	private By play= By.xpath("//*[@title=\"Play\"]");
 	private By pause= By.xpath("//*[@title=\"Pause\"]");
 	private By replay= By.xpath("//*[@title=\"Replay\"]");
+	
+	private By manageContenticon= By.xpath("//div[@class='icon dripicons']");
+	private By tabCatalogItem= By.xpath("//md-tab-item//span[contains(text(),'Catalog Items')]");
+	private By bundleClick= By.xpath("//span[@title='Auto_Bundle']");
+	private By learningPathClick= By.xpath("//span[@title='Auto_LearningPath']");
+	private By videoClick= By.xpath("//h3[@title='Auto_MP4']");
+	private By homeLink=By.xpath("//a[contains(text(),'Catalog Items')]");
+	private By closeBtn=By.xpath("//i[@class='icon dripicons-cross icon30 font-size-30 ng-scope']");
 	public videoTesterPage(WebDriver driver) {
 		super(driver);
 	}
@@ -56,4 +64,93 @@ public class videoTesterPage extends FractalBasePage{
 			print("Exception on video player");
 		}
 	}
+		public void verifyAdminVideoTester() throws InterruptedException {
+			Actions	actions1 = new Actions(driver);
+			click(manageContenticon);
+			wait(2);
+			click(tabCatalogItem);
+			wait(2);
+			WebElement element = driver.findElement(bundleClick);
+			actions1.moveToElement(element).perform();
+			click(bundleClick);
+				WebElement element1 = driver.findElement(videoClick);
+				actions1.moveToElement(element1).perform();
+				click(videoClick);
+				wait(5);
+				driver.switchTo().frame(0);
+				wait(10);
+				click(videoPlayer);
+				print("Video is playing.");
+				wait(20);
+				if(elementExist(adminPause)) {
+					element = driver.findElement(adminPause);
+					actions1 = new Actions(driver); 
+					actions1.moveToElement(element1).perform();
+					click(adminPause);
+					print("Video is paused.");
+					wait(10);
+			click(closeBtn);
+			wait(5);
+			WebElement element2 = driver.findElement(homeLink);
+			actions1.moveToElement(element2).perform();
+			click(homeLink);
+			wait(5);
+			click(tabCatalogItem);
+			wait(5);
+			WebElement element3 = driver.findElement(learningPathClick);
+			actions1.moveToElement(element3).perform();
+			click(learningPathClick);
+			wait(5);
+			
+			actions1.moveToElement(element1).perform();
+			click(videoClick);
+			wait(5);
+			click(closeBtn);
+			wait(2);
+				}
+			
+			
+		}
+	/*		public By searchFieldLearnObj = By.xpath("//input[@id='searchObject']");
+			private By Iblvideo=By.xpath("//*[text()='Videos Sample']");
+			private By videoPlayer=By.xpath("//*[@class='vjs-big-play-button']");
+	*/		private By adminPlay= By.xpath("//*[@class='vjs-play-control vjs-control vjs-button vjs-paused']");
+			private By adminPause= By.xpath("//*[@class='vjs-play-control vjs-control vjs-button vjs-playing']");
+	//		ManageContentItemCodesPage ManageContentItemCodesPage = null;
+			
+	/*		public void verifyVideoTester() throws InterruptedException {
+				Actions	actions = new Actions(driver); 
+				ManageContentItemCodesPage.goToManagecontent();
+				clear(searchFieldLearnObj);
+				enterData("Videos", searchFieldLearnObj);
+				wait(5);
+				try{
+					WebElement element = driver.findElement(Iblvideo);
+					actions.moveToElement(element).perform();
+					click(Iblvideo);
+					wait(5);
+					driver.switchTo().frame(0);
+					wait(10);
+					click(videoPlayer);
+					print("Video is playing.");
+					wait(20);
+					if(elementExist(pause)) {
+						element = driver.findElement(pause);
+						actions = new Actions(driver); 
+						actions.moveToElement(element).perform();
+						click(pause);
+						print("Video is paused.");
+						wait(10);
+					}
+					else {
+						print("Unknown Error on video player");
+					}
+				}catch(Exception e) {
+					print("Exception on video player");
+				}
+			}
+
+*/
+
 }
+		
--- src/test/java/com/origin/fractal/testauto/test/testsuites/WebTestVideoTester.java
+++ src/test/java/com/origin/fractal/testauto/test/testsuites/WebTestVideoTester.java
@@ -5,13 +5,14 @@ import java.io.IOException;
 import org.testng.annotations.Test;
 
 import com.origin.fractal.testauto.DataManager;
+import com.origin.fractal.testauto.steps.MenuSteps;
 import com.origin.fractal.testauto.steps.VideoTesterSteps;
 import com.origin.fractal.testauto.test.FractalBaseWebTest;
 import com.wv.auto.framework.BrowserFactory;
 import com.wv.auto.framework.utils.Reporter;
 
 public class WebTestVideoTester extends FractalBaseWebTest {
-	@Test(dataProviderClass=DataManager.class, dataProvider = "browers", groups = { "pilot" }, enabled = true, priority = 0 ,
+	@Test(dataProviderClass=DataManager.class, dataProvider = "browers", groups = { "pilot" }, enabled = true , priority = 0 ,
 			description = "video testing")
 	public void verifyvideo(String row, String strBrowserName) throws InterruptedException {
 		driver = BrowserFactory.getBrowser(strBrowserName);
@@ -19,8 +20,21 @@ public class WebTestVideoTester extends FractalBaseWebTest {
 		login(driver);
 		VideoTesterSteps VideoTesterSteps=new VideoTesterSteps(driver);
 		VideoTesterSteps.verifyVideoTester();
-		Reporter.writeSummary("TCID_Video, Verify the video gets launched succesfully," +  VideoTesterSteps.getResult() );
+		Reporter.writeSummary("TCID_Video, Verify the User video gets launched succesfully," +  VideoTesterSteps.getResult() );
+	
+	}
+	@Test(dataProviderClass=DataManager.class, dataProvider = "browers", groups = { "pilot" }, enabled = true, priority = 0 ,
+			description = "video testing")
+	public void verifyAdminvideo(String row, String strBrowserName) throws InterruptedException, IOException {
+		driver = BrowserFactory.getBrowser(strBrowserName);
+		loginToContentAdmin(driver);
+		MenuSteps menuSteps = new MenuSteps(driver);
+		VideoTesterSteps VideoTesterSteps=new VideoTesterSteps(driver);
+		menuSteps.clickMenu();
+		VideoTesterSteps.verifyAdminVideoTester();
+		Reporter.writeSummary("TCID_Video, Verify the Admin video gets launched succesfully," +  VideoTesterSteps.getResult() );
 	
 	}
 }
+
 
