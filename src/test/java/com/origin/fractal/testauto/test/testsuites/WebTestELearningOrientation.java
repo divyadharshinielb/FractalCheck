fcf278c92a9e24ee11a09c11346bb641f1ac402e
DESKTOP-LJ29EIC <team-wvi@originlearning.com>
2020-08-21 20:40:57 +0530

    Integrated FR1-2359 (eLearning Orientation Admin side) automation code with the existing code - Baskaran

--- src/test/java/com/origin/fractal/testauto/test/testsuites/WebTestELearningOrientation.java
+++ src/test/java/com/origin/fractal/testauto/test/testsuites/WebTestELearningOrientation.java
@@ -25,10 +25,10 @@ public class WebTestELearningOrientation extends FractalBaseWebTest{
 		eLearningOrientation.verifyELearningOrientation();
 		Reporter.writeSummary("FR1-2539_eLearnOrient_TC-001, Verify the admin able to see orientation Modes Auto/Landscape/Portrait in elearning," +  eLearningOrientation.getResult()); 
 		eLearningOrientation.verifyOrientationSelection();
-		Reporter.writeSummary("FR1-2539_eLearnOrient_TC-02, Verify the admin able to select the orientation Auto/Landscape/Portrait in elearning," +  eLearningOrientation.getResult()); 
+		Reporter.writeSummary("FR1-2539_eLearnOrient_TC-002, Verify the admin able to select the orientation Auto/Landscape/Portrait in elearning," +  eLearningOrientation.getResult()); 
 		eLearningOrientation.verifyScormOrientation();
-		Reporter.writeSummary("FR1-2539_eLearnOrient_TC-03, Verify the admin able to select the orientation Auto/Landscape/Portrait and save the elearning with Scrome1.2 package," +  eLearningOrientation.getResult()); 
+		Reporter.writeSummary("FR1-2539_eLearnOrient_TC-003, Verify the admin able to select the orientation Auto/Landscape/Portrait and save the elearning with Scrome1.2 package," +  eLearningOrientation.getResult()); 
 		eLearningOrientation.verifyXapiOrientation();
-		Reporter.writeSummary("FR1-2539_eLearnOrient_TC-04, Verify the admin able to select the orientation Auto/Landscape/Portrait and save the elearning with Xapi package," +  eLearningOrientation.getResult()); 
+		Reporter.writeSummary("FR1-2539_eLearnOrient_TC-004, Verify the admin able to select the orientation Auto/Landscape/Portrait and save the elearning with Xapi package," +  eLearningOrientation.getResult()); 
 	}
 }
