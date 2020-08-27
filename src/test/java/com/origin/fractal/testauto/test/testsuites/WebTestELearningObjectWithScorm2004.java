a0ad967b405dd0425431e4acbff2e5f7717eb3f2
DESKTOP-LJ29EIC <team-wvi@originlearning.com>
2020-08-27 19:47:50 +0530

    Automation environment - Updated testng.xml  for FR1-2207 & FR1-2364 with the existing code

--- src/test/java/com/origin/fractal/testauto/test/testsuites/WebTestELearningObjectWithScorm2004.java
+++ src/test/java/com/origin/fractal/testauto/test/testsuites/WebTestELearningObjectWithScorm2004.java
@@ -23,20 +23,20 @@ public class WebTestELearningObjectWithScorm2004 extends FractalBaseWebTest{
 		siteAdminLoginwithcookies(driver);
 		ELearningObjectWithScorm2004Steps scorm2004= new ELearningObjectWithScorm2004Steps(driver);
 		scorm2004.verifyCreateELearningObjectWithscorm2004();
-		Reporter.writeSummary("FR1-2364_TC-001,Verify that Site admin is able to Create an eLearning object with Scorm 2004," +scorm2004.getResult());
+		Reporter.writeSummary("FR1-2364_ElearnScorm2004_TC-001,Verify that Site admin is able to Create an eLearning object with Scorm 2004," +scorm2004.getResult());
 		scorm2004.verifyEditAndCancelELearningObjectWithscorm2004();
-		Reporter.writeSummary("FR1-2364_TC-002,Verify that Site admin is able to click Cancel and Edit the eLearning object with Scorm 2004," +scorm2004.getResult());
+		Reporter.writeSummary("FR1-2364_ElearnScorm2004_TC-002,Verify that Site admin is able to click Cancel and Edit the eLearning object with Scorm 2004," +scorm2004.getResult());
 		scorm2004.verifyPreviewscorm2004();
-		Reporter.writeSummary("FR1-2364_TC-003,Verify that Site admin is able to Preview the eLearning object with Scorm 2004," +scorm2004.getResult());
+		Reporter.writeSummary("FR1-2364_ElearnScorm2004_TC-003,Verify that Site admin is able to Preview the eLearning object with Scorm 2004," +scorm2004.getResult());
 		scorm2004.verifyInvalidscorm2004();
-		Reporter.writeSummary("FR1-2364_TC-004,Verify that Site admin is Not able to Upload the invalid Scorm file," +scorm2004.getResult());
+		Reporter.writeSummary("FR1-2364_ElearnScorm2004_TC-004,Verify that Site admin is Not able to Upload the invalid Scorm file," +scorm2004.getResult());
 		scorm2004.createandEditCatalogBundleWitheLearningWithscorm2004("Add scorm2004");
-		Reporter.writeSummary("FR1-2364_TC-005,Verify that Site admin is able to map the eLearning Object with Scorm 2004 to Bundle Catalog item," +scorm2004.getResult());
+		Reporter.writeSummary("FR1-2364_ElearnScorm2004_TC-005,Verify that Site admin is able to map the eLearning Object with Scorm 2004 to Bundle Catalog item," +scorm2004.getResult());
 		login1(driver);
 		scorm2004.userSearchElearningobjectWithscorm2004AndLaunch();
-		Reporter.writeSummary("FR1-2364_TC-006,Verify that User is able to see the Scorm 2004 eLearning Object in Bundle Catalog item," +scorm2004.getResult());
+		Reporter.writeSummary("FR1-2364_ElearnScorm2004_TC-006,Verify that User is able to see the Scorm 2004 eLearning Object in Bundle Catalog item," +scorm2004.getResult());
 		siteAdminLoginwithcookies(driver);
 		scorm2004.verifyDeleteELearnignobjectWithscorm2004();
-		Reporter.writeSummary("FR1-2364_TC-007,Verify that Site admin is able Delete an eLearning object with Scorm 2004," +scorm2004.getResult());
+		Reporter.writeSummary("FR1-2364_ElearnScorm2004_TC-007,Verify that Site admin is able Delete an eLearning object with Scorm 2004," +scorm2004.getResult());
 	}
 }
