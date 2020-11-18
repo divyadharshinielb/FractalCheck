9d288b33eaeb2e7a9a22ffba4bb87c6719ccc6a5
DESKTOP-LJ29EIC <team-wvi@originlearning.com>
2020-11-18 12:22:54 +0530

    Updated files for FR1-2772

--- src/test/java/com/origin/fractal/testauto/test/testsuites/WebTestCatalogVersioning.java
+++ src/test/java/com/origin/fractal/testauto/test/testsuites/WebTestCatalogVersioning.java
@@ -18,7 +18,7 @@ import com.wv.auto.framework.utils.Reporter;
  * Updated Date: 16-Nov-20
  */
 public class WebTestCatalogVersioning extends FractalBaseWebTest{
-	@Test(dataProviderClass=DataManager.class, dataProvider = "browers", groups = { "pilot"}, enabled = true,priority=1, description = "Verification for all the 31 email templates")
+	@Test(dataProviderClass=DataManager.class, dataProvider = "browers", groups = { "pilot"}, enabled = true,priority=1)
 	public void TestCatalogVersioning(String row, String strBrowserName) throws IOException{
 		driver = BrowserFactory.getBrowser(strBrowserName);
 		siteAdminLoginwithcookies(driver); 
