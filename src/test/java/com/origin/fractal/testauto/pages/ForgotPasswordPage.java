aef22272aeac3a07b15d7577e80d88f5c796c783
divyadharshini <divyadharshini.r@originlearning.com>
2021-11-09 12:59:37 +0530

    Staging weekly

--- src/test/java/com/origin/fractal/testauto/pages/ForgotPasswordPage.java
+++ src/test/java/com/origin/fractal/testauto/pages/ForgotPasswordPage.java
@@ -7,8 +7,8 @@ import com.origin.fractal.testauto.FractalBasePage;
 
 public class ForgotPasswordPage extends FractalBasePage{
 	private By forgotPasswordLink  = By.xpath(".//a[contains(text(),'Forgot password?')]");
-	private By txtInputEmail  = By.xpath(".//input[contains(@class,'mui--is-untouched mui--is-dirty mui--is-empty')]");
-	private By btnSend  = By.xpath(".//input[contains(@class,'btn w100 primary-colr send_btn reset')]");
+	private By txtInputEmail  = By.xpath("//input[@name='emailid']");
+	private By btnSend  = By.xpath("//input[@value='SEND']");
 	//private By forgotPasswordLink  = By.xpath("");
 	//private By forgotPasswordLink  = By.xpath("");
 	private By prelogin = By.xpath("//a[contains(text(),'LOGIN')]");
