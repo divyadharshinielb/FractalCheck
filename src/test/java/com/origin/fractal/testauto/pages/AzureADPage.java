477ea5737297bbc67a490693bd6e28b6085921ff
Mahesh_Geddam <mahesh.g@originlearning.com>
2021-12-30 17:07:00 +0530

    AzureAD code added

---
+++ src/test/java/com/origin/fractal/testauto/pages/AzureADPage.java
@@ -0,0 +1,59 @@
+package com.origin.fractal.testauto.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import com.origin.fractal.testauto.FractalBasePage;
+/*
+ * File: AzureADPage.java
+ * Purpose: Azure page (FR1-5236) - Step level code
+ * Created by: Mahesh G
+ * Updated on: 30-12-2021
+ */
+public class AzureADPage extends FractalBasePage  { 
+private By lblSocialLogin = By.xpath("//div[contains(text(),'Enable/Disable Subscription')]");
+private By lblSSOSwitch= By.xpath("//body/div[1]/div[1]/div[2]/div[3]/div[3]/div[1]/div[2]/div[1]/div[1]/md-tabs[1]/md-tabs-content-wrapper[1]/md-tab-content[1]/div[1]/ng-include[1]/form[1]/div[1]/div[11]/div[1]/div[1]/span[3]/div[2]/div[1]/md-switch[1]/div[1]/div[2]/div[1]");
+private By lblPrivacy= By.xpath("//a[contains(text(),'Privacy')]");	
+private By btnSave= By.xpath("//body/div[1]/div[1]/div[2]/div[3]/div[3]/div[1]/div[2]/div[1]/div[1]/md-tabs[1]/md-tabs-content-wrapper[1]/md-tab-content[1]/div[1]/ng-include[1]/form[1]/div[1]/div[22]/div[2]/button[1]");
+private By ssoBtn= By.xpath("//button[@id='sso_button_color']");
+public AzureADPage(WebDriver driver) {
+		super(driver);
+	}
+/* Method: enableSSO();
+ * purpose:verify enableSSO page
+ */
+	public void enableSSO() {
+		WebElement element = driver.findElement(lblSocialLogin); 
+		Actions actions = new Actions(driver); 
+		actions.moveToElement(element);
+		actions.perform();
+		wait(6);
+		click(lblSSOSwitch);
+		wait(5);
+		WebElement element1 = driver.findElement(lblPrivacy); 
+		Actions actions1 = new Actions(driver); 
+		actions1.moveToElement(element1);
+		actions1.perform();
+		wait(6);
+		click(btnSave);
+		wait(6);
+	}
+	/* Method: verifySSOBtn();
+	 * purpose:verify SSOBtn
+	 */
+	public void verifySSOBtn() {
+		wait(6);
+		if(elementExist(ssoBtn)) {
+			wait(5);
+			click(ssoBtn);
+			wait(10);
+		}
+		else {
+			print("SSO button is in disbale mode");
+			
+		}
+	}
+	
+}
