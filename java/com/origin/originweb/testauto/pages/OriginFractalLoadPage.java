248f0dfe443f28ad312a662b314e8c348158f101
Mahesh G <mahesh.g@originlearning.com>
2022-03-15 11:10:41 +0000

    OriginFractalLoadPage.java edited online with Bitbucket by sujith

--- java/com/origin/originweb/testauto/pages/OriginFractalLoadPage.java
+++ java/com/origin/originweb/testauto/pages/OriginFractalLoadPage.java
@@ -1,33 +1,49 @@
 package com.origin.originweb.testauto.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import com.origin.originweb.testauto.OriginFractalWebBasePage;
+import com.wv.auto.framework.utils.TimeManager;
 /* File Name: OriginFractalLoadPage.java
  * Purpose: https://originfractal.com/ - Verifying page loading time
  * Created by: Vigneshwaran R
  * Created on: 10-Nov-2020
+ * Updated on: 13-Jan-21
  */
 public class OriginFractalLoadPage extends OriginFractalWebBasePage {
-	private By cookiesBtn=By.xpath("//button[@id='onesignal-slidedown-cancel-button']");//*[@id='euCookieAcceptWP']
+	private By cookiesBtn=By.xpath("//a[text()='Accept']");//Updated on 26-Nov-20
+	//	private By btnUnderstand=By.xpath("//button[text()='I Understand']"); //Added on 26-Nov-20
 	private By headerLogo=By.xpath("//*[@class='logo']");
+	private By newsSlider=By.xpath("//*[@id='normal-slidedown']");//Added on 26-Nov-20
+	private By btnCancelnewsSlider=By.xpath("//*[@id='normal-slidedown']//button[text()='Cancel']");//Added on 26-Nov-20
 	// Heading 
-	private By headerFeature	= By.xpath("(//UL[@class='navigation clearfix']//a)[1]");
-	private By headerPricing	= By.xpath("(//UL[@class='navigation clearfix']//a)[2]");
-	private By headerTrailSign	= By.xpath("(//UL[@class='navigation clearfix']//a)[3]");
+	private By headerSolution     = By.xpath("//a[contains(text(),'Solutions')]");
+	private By headerSolutionSub1 = By.xpath("(//UL[@class='navigation clearfix']//a)[1]");
+	private By headerSolutionSub2 = By.xpath("(//UL[@class='navigation clearfix']//a)[2]");
+	private By headerSolutionSub3 = By.xpath("(//UL[@class='navigation clearfix']//a)[3]");
+	private By headerProduct     = By.xpath("//a[contains(text(),'Product')]");
+	private By headerFeature     = By.xpath("//header/div[1]/div[1]/div[2]/div[1]/nav[1]/div[2]/ul[1]/li[2]/ul[1]/li[1]/a[1]");
+	private By headerPricing	= By.xpath("//header/div[1]/div[1]/div[2]/div[1]/nav[1]/div[2]/ul[1]/li[2]/ul[1]/li[2]/a[1]");
+	private By headerTrailSign	= By.xpath("//header/div[1]/div[1]/div[2]/div[1]/nav[1]/div[2]/ul[1]/li[2]/ul[1]/li[3]/a[1]");
 	private By headerBlog		= By.xpath("(//UL[@class='navigation clearfix']//a)[4]");
-	private By headerPhilosophy	= By.xpath("(//UL[@class='navigation clearfix']//a)[5]");
-	private By headerAboutUs	= By.xpath("(//UL[@class='navigation clearfix']//a)[6]");
-	private By headerContact	= By.xpath("(//UL[@class='navigation clearfix']//a)[7]");
+	private By headerResources	= By.xpath("(//UL[@class='navigation clearfix']//a)[5]");// Added ON 13-Jan-20
+	private By headerPhilosophy	= By.xpath("//header/div[1]/div[1]/div[2]/div[1]/nav[1]/div[2]/ul[1]/li[4]/ul[1]/li[2]/a[1]");
+	private By headerCareer 	= By.xpath("//header/div[1]/div[1]/div[2]/div[1]/nav[1]/div[2]/ul[1]/li[4]/ul[1]/li[3]/a[1]");
+	private By headerAboutUs1	= By.xpath("//a[contains(text(),'About Us')]");
+	private By headerAboutUs	= By.xpath("(//UL[@class='navigation clearfix']//a)[7]");
+	private By headerContact	= By.xpath("//header/div[1]/div[1]/div[2]/div[1]/nav[1]/div[2]/ul[1]/li[4]/ul[1]/li[4]/a[1]");
+	private By unpluggedBanner	= By.xpath("//a[@href='https://originfractal.com/unplugged']");// Added ON 13-Jan-20
 	//Home page
-	private By postedBlog1		= By.xpath("(//*[@class='entry-thumb-wrapper'])[1]");
-	private By postedBlog2		= By.xpath("(//*[@class='entry-thumb-wrapper'])[2]");
-	private By postedBlog3 		= By.xpath("(//*[@class='entry-thumb-wrapper'])[3]");
+	private By postedBlog1		= By.xpath("((//*[@class='kc-list-item-3'])[1]//a)[text()='Read more ']");//Updated on 18-Dec-20
+	private By postedBlog2		= By.xpath("((//*[@class='kc-list-item-3'])[2]//a)[text()='Read more ']");//Updated on 18-Dec-20
+	private By postedBlog3 		= By.xpath("((//*[@class='kc-list-item-3'])[3]//a)[text()='Read more ']");//Updated on 18-Dec-20
 	private By banner 			= By.xpath("//*[@class='page-title page-titlex']");
 	//Blog page
-	private By search 			= By.xpath("(//input)[2]");
+	private By search 			= By.xpath("//*[@placeholder='Search Here..']");//Added on 14-Dec-12
 	//Footer
 	private By lblHome			= By.xpath("(//*[@class='widgets_wrapper']//a)[1]");
 	private By lblLXPLearner	= By.xpath("(//*[@class='widgets_wrapper']//a)[7]");
@@ -38,19 +54,39 @@ public class OriginFractalLoadPage extends OriginFractalWebBasePage {
 	private By txtDomain		= By.xpath("//h2//strong[text()='Domain Expert or']");
 	private By linkAppAndroid	= By.xpath("(//*[@class='social-app']//a)[1]");
 	private By linkAppiOS		= By.xpath("(//*[@class='social-app']//a)[2]");
+	private By popUp		= By.xpath("//*[@class='fractal-ent']");
+//	private By blog		= By.xpath("//footer[@id='Footer']");
+	private By linkblog		= By.xpath("//a[contains(text(),'Blog')]");
 	public OriginFractalLoadPage(WebDriver driver) {
 		super(driver);
 		pageName="originFractal";
 	}	
+
+	/* Function Name: clickPopUp()
+	 * Purpose:click PopUp Image using Sukili logic
+	 */
+	public void clickPopUp() {
+		WebElement banner = driver.findElement(By.name("image-map1"));
+		WebElement area = banner.findElement(popUp);
+
+		JavascriptExecutor executor = (JavascriptExecutor)driver;
+		executor.executeScript("arguments[0].click();", area);
+
+	}
+
 	/* Function Name: goToURL()
 	 * Purpose: Navigate the URL and close the cookies
 	 */
 	public void goToURL() {
 		goTo(getLabel("webUrl"));
-		if(elementExist(cookiesBtn)) {
-			click(cookiesBtn);
+		wait(5);
+		if(elementExist(newsSlider)) {
+			moveElementFocusandClick(btnCancelnewsSlider);
 		}
-		wait(2);
+		if(elementExist(cookiesBtn)) {
+			moveElementFocusandClick(cookiesBtn);
+			//			moveElementFocusandClick(btnUnderstand);
+		}
 	}
 	/*Function Name: gotoHome()
 	 * purpose: Click on home icon
@@ -66,18 +102,44 @@ public class OriginFractalLoadPage extends OriginFractalWebBasePage {
 	public boolean verifyHomeLogo() {
 		return clickAndcheckLoading(headerLogo,getLabel("webUrl"));
 	}
-
+	/* Function Name: verifyHeaderSolution()
+	 * Purpose: Verify the page loading time
+	 */
+	public boolean verifyHeaderSolution() {
+		return clickAndcheckLoading(headerSolution,getLabel("soutionUrl"));
+	}
+	/* Function Name: verifyHeaderSolutionSub1()
+	 * Purpose: Verify the page loading time
+	 */
+public boolean verifyHeaderSolutionSub1() {
+	click(headerSolution);
+	return clickAndcheckLoading(headerSolutionSub1,getLabel("soutionUrl"));
+}
+/* Function Name: verifyHeaderSolutionSub2()
+ * Purpose: Verify the page loading time
+ */
+public boolean verifyHeaderSolutionSub2() {
+	click(headerSolution);
+	return clickAndcheckLoading(headerSolutionSub2,getLabel("soutionUrlSub1"));
+}
+/* Function Name: verifyHeaderSolutionSub3()
+ * Purpose: Verify the page loading time
+ */
+public boolean verifyHeaderSolutionSub3() {
+	click(headerSolution);
+	return clickAndcheckLoading(headerSolutionSub3,getLabel("soutionUrlSub2"));
+}
 	/* Function Name: verifyHeaderFeature()
 	 * Purpose: Verify the page loading time
 	 */
 	public boolean verifyHeaderFeature() {
+		click(headerProduct);
 		return clickAndcheckLoading(headerFeature,getLabel("fraturesUrl"));
 	}
-
 	/* Function Name: verifyHeaderPricing()
 	 * Purpose: Verify the page loading time
 	 */
-	public boolean verifyHeaderPricing() {
+	public boolean verifyHeaderPricing(){
 		return clickAndcheckLoading(headerPricing,getLabel("pricingUrl"));
 	}
 
@@ -87,44 +149,74 @@ public class OriginFractalLoadPage extends OriginFractalWebBasePage {
 	public boolean verifyHeaderTrail() {
 		return clickAndcheckLoading(headerTrailSign,getLabel("trialUrl"));
 	}
-	
+
 	/* Function Name: verifyHeaderBlog()
 	 * Purpose: Verify the page loading time
 	 */
 	public boolean verifyHeaderBlog(){
 		return clickAndcheckLoading(headerBlog,getLabel("blogUrl"));
 	}
+
+	//Added 0n 13-Jan-21
+	/* Function Name: verifyHeaderResources()
+	 * Purpose: Verify the page loading time
+	 */
+	public boolean verifyHeaderResources(){
+		return clickAndcheckLoading(headerResources,getLabel("resourceUrl"));
+	}
+
+	/* Function Name: verifyunpluggedBanner()
+	 * Purpose: Verify the page loading time
+	 */
+	public boolean verifyunpluggedBanner(){
+		moveElementFocus(unpluggedBanner);
+		return clickAndcheckLoadingOtherTab(unpluggedBanner,getLabel("unpluggedUrl"));
+	}
+	//Ends
 	
 	/* Function Name: verfiySearchFunction()
 	 * Purpose: Search and Verify the page loading time
 	 */
 	public boolean verfiySearchFunction() {
+		
+		wait(3);//Added on 14-Dec-12
 		enterData("Fractal",search);
 		driver.findElement(search).sendKeys(Keys.RETURN);
-		return elementExist(banner);
+		return elementExist(banner) && TimeManager.seconds<=20; //Updated on 3-Dec-20
 	}
 
 	/* Function Name: verifyHeaderPhilosophy()
 	 * Purpose: Verify the page loading time
 	 */
 	public boolean verifyHeaderPhilosophy() {
+		click(headerAboutUs1);
 		return clickAndcheckLoading(headerPhilosophy,getLabel("philosophyUrl"));
 	}	
-	
+
 	/* Function Name: verifyHeaderAbout()
 	 * Purpose: Verify the page loading time
 	 */
-	public boolean verifyHeaderAbout(){
-		return clickAndcheckLoading(headerAboutUs,getLabel("aboutUsUrl"));
-	}
 	
+	public boolean verifyHeaderAbout() {
+		print("1");
+		return clickAndcheckLoading(headerAboutUs1,getLabel("aboutUsUrl"));
+	}  
+	
+	/* Function Name: verifyHeaderCareer()
+	 * Purpose: Verify the page loading time
+	 */
+	public boolean verifyHeaderCareer() {
+		click(headerAboutUs1);
+		return clickAndcheckLoading(headerCareer,getLabel("careerUrl"));
+	}
+
 	/* Function Name: verifyHeaderContact()
 	 * Purpose: Verify the page loading time
 	 */
 	public boolean verifyHeaderContact(){
 		return clickAndcheckLoading(headerContact,getLabel("contactUrl"));
 	}
-	
+
 	/* Function Name: verifyPostBlog1()
 	 * Purpose: Verify the page loading time
 	 */
@@ -136,18 +228,18 @@ public class OriginFractalLoadPage extends OriginFractalWebBasePage {
 	/* Function Name: verifyPostBlog2()
 	 * Purpose: Verify the page loading time
 	 */
-	public boolean verifyPostBlog2() {
+	public boolean verifyPostBlog2(){
 		moveElementFocus(postedBlog2);
 		return clickAndcheckLoading(postedBlog2,banner);
 	}
 	/* Function Name: verifyConsultingSubManu()
 	 * Purpose: Verify the page loading time
 	 */
-	public boolean verifyPostBlog3() {
+	public boolean verifyPostBlog3(){
 		moveElementFocus(postedBlog3);
 		return clickAndcheckLoading(postedBlog3,banner);
 	}
-	
+
 	/* Function Name: verifylblHome()
 	 * Purpose: Verify the page loading time
 	 */
@@ -155,7 +247,7 @@ public class OriginFractalLoadPage extends OriginFractalWebBasePage {
 		moveElementFocus(lblHome);
 		return clickAndcheckLoading(lblHome,getLabel("webUrl"));
 	}
-	
+
 	/* Function Name: verifylblLxpLearners()
 	 * Purpose: Verify the page loading time
 	 */
@@ -163,25 +255,23 @@ public class OriginFractalLoadPage extends OriginFractalWebBasePage {
 		moveElementFocus(lblLXPLearner);
 		return clickAndcheckLoading(lblLXPLearner,txtLearner);
 	}
-	
+
 	/* Function Name: verifylblLxpEnterprises()
 	 * Purpose: Verify the page loading time
 	 */
-	public boolean verifylblLxpEnterprises() {
-		wait(1);
+	public boolean verifylblLxpEnterprises(){
 		moveElementFocus(lblLXPEnterPrises);
 		return clickAndcheckLoading(lblLXPEnterPrises,txtEnterPrise);
 	}
-	
+
 	/* Function Name: verifylblLxpDomain()
 	 * Purpose: Verify the page loading time
 	 */
 	public boolean verifylblLxpDomain() {
-		wait(1);
 		moveElementFocus(lblLXPDomainExp);
 		return clickAndcheckLoading(lblLXPDomainExp,txtDomain);
 	}
-	
+
 	/* Function Name: verifyAndroidPlayStore()
 	 * Purpose: Verify the page loading time
 	 */
@@ -189,12 +279,20 @@ public class OriginFractalLoadPage extends OriginFractalWebBasePage {
 		moveElementFocus(linkAppAndroid);
 		return clickAndcheckLoadingOtherTab(linkAppAndroid,getLabel("playStoreUrl"));
 	}
-	
+
 	/* Function Name: verifyiOSAppStore()
 	 * Purpose: Verify the page loading time
 	 */
 	public boolean verifyiOSAppStore(){
 		moveElementFocus(linkAppiOS);
-		return clickAndcheckLoading(linkAppiOS,getLabel("appStoreUrl"));
+		return clickAndcheckLoadingOtherTab(linkAppiOS,getLabel("appStoreUrl"));
 	}
+	/* Function Name: verifyBlog()
+	 * Purpose: Verify the page loading time
+	 */
+	public boolean verifyBlog(){
+		moveElementFocus(linkblog);
+		return clickAndcheckLoading(linkblog,getLabel("blogUrl"));
+	}
+	
 }
